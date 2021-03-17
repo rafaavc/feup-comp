322dd2547ad6e8de120bdafcb54ba093f3253fa4
rafaavc <rafavc2508@gmail.com>
2021-03-17 12:09:49 +0000

    Catching errors and printing

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,29 @@ import pt.up.fe.comp.jmm.report.Report;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.io.StringReader;
+import java.util.List;
 
+import pt.up.fe.comp.jmm.report.ReportType;
+import pt.up.fe.comp.jmm.report.Stage;
 import pt.up.fe.specs.util.SpecsIo;
 
 public class Main implements JmmParser {
 
 
 	public JmmParserResult parse(String jmmCode) {
-
 		try {
 			// Replace with parser class
 		    Jmm parser = new Jmm(new StringReader(jmmCode));
     		SimpleNode root = parser.Program(); // returns reference to root node
             	
     		root.dump(""); // prints the tree on the screen
-    	
-    		return new JmmParserResult(root, new ArrayList<>());
-		} catch(ParseException e) {
-			throw new RuntimeException("Error while parsing", e);
+
+    		return new JmmParserResult(root, parser.getReports());
+
+		} catch(Exception e) {  //  Only for the rare case when an exception isn't a ParseException (parse exceptions are caught before)
+			List<Report> reports = new ArrayList<>();
+			reports.add(new Report(ReportType.ERROR, Stage.SYNTATIC, 0, e.getMessage()));
+			return new JmmParserResult(null, reports);
 		}
 	}
 
@@ -31,6 +36,11 @@ public class Main implements JmmParser {
         System.out.println("Executing with args: " + Arrays.toString(args));
 		String jmmCode = SpecsIo.read(args[0]);
 		JmmParser parser = new Main();
-		parser.parse(jmmCode);
+		JmmParserResult result = parser.parse(jmmCode);
+
+		List<Report> reports = result.getReports();
+		for (Report report : reports.subList(0, Math.min(reports.size(), 10))) {
+			System.out.println(report.getMessage());
+		}
     }
 }
