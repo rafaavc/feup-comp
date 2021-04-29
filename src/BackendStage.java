2a0147872dcba56fe2972153da8287f1bb5de4e9
DanielGSilva <dg.sarmento2000@gmail.com>
2021-04-29 19:00:40 +0100

    Start Jasmin translation

--- src/BackendStage.java
+++ src/BackendStage.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import org.specs.comp.ollir.ClassUnit;
 import org.specs.comp.ollir.OllirErrorException;
+import org.specs.comp.ollir;
 
 import pt.up.fe.comp.jmm.jasmin.JasminBackend;
 import pt.up.fe.comp.jmm.jasmin.JasminResult;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.Stage;
 import pt.up.fe.specs.util.SpecsIo;
+
 
 /**
  * Copyright 2021 SPeCS.
@@ -41,7 +45,7 @@ public class BackendStage implements JasminBackend {
             ollirClass.show(); // print to console main information about the input OLLIR
 
             // Convert the OLLIR to a String containing the equivalent Jasmin code
-            String jasminCode = ""; // Convert node ...
+            String jasminCode = getJasminCode(ollirClass); // Convert node ...
 
             // More reports from this stage
             List<Report> reports = new ArrayList<>();
@@ -53,6 +57,90 @@ public class BackendStage implements JasminBackend {
                     Arrays.asList(Report.newError(Stage.GENERATION, -1, -1, "Exception during Jasmin generation", e)));
         }
 
+    }
+
+    private String getJasminCode(ClassUnit classUnit) {
+        StringBuilder code = new StringBuilder();
+
+        code.append(".class " + classUnit.getClassAccessModifier().toString() + " " + classUnit.getClassName() );
+
+        code.append(".super " + classUnit.getSuperClass());
+
+        //constructor
+        code.append(".method " + classUnit.getClassAccessModifier().toString() + " <init>()V");
+        code.append("aload_0");
+        code.append("invokenonvirtual java/lang/Object/<init>()V");
+        code.append("return");
+        code.append(".end method");
+
+        for (Method m : classUnit.getMethods()) {
+            String declaration = ".method " + m.getMethodAccessModifier().toString() + " ";
+
+            if (m.isFinalMethod()) {
+                declaration += "final ";
+            }
+
+            if (m.isStaticMethod()) {
+                declaration += "static ";
+            }
+
+            declaration += m.getMethodName() + "(";
+
+            for (Element e : m.getParams()) {
+                declaration += getElementType(e.getType()) + ",";
+            }
+            if (m.getParams().size() > 0) {
+                declaration = declaration.substring(0, declaration.length() -1);
+            }
+
+            declaration += ")" + getElementType(m.getReturnType());
+
+            code.append(declaration);
+
+            code.append(".limit locals 99");
+            code.append(".limit stack 99");
+
+            for (Instruction i : m.getInstructions()) {
+
+            }
+        }
+
+
+
+        System.out.println(code.toString());
+
+        return code.toString();
+    }
+
+    private String getElementType(Type e) {
+        ElementType eType = e.getTypeOfElement();
+
+        switch (eType) {
+            case INT32:
+                return "I";
+                break;
+            case BOOLEAN:
+                return "x";
+                break;
+            case ARRAYREF:
+                return "[";
+                break;
+            case OBJECTREF:
+                return "x";
+                break;
+            case CLASS:
+                return "x";
+                break;
+            case THIS:
+                return "x";
+                break;
+            case STRING:
+                return "x";
+                break;
+            case VOID:
+                return "V";
+                break;
+        }
     }
 
 }
